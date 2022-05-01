937447561b83afd82993b7701123bedd0715a418
Marc Lopez <99056015+Marclopez11@users.noreply.github.com>
2022-05-01 13:20:27 +0200

    Ej1

---
+++ Ej1.sql
@@ -0,0 +1,15 @@
+﻿INSERT INTO despachos (capacidad)
+VALUES (1),(1),(1),(1),(1),(1),(1),(1),(1),(1);
+
+
+INSERT INTO directores (dni, nombre_apellidos, id_despacho, id_dnijefe)
+VALUES ("4768243A", "Marc lopez", 1, "4768243A"),
+("4738241A", "Marc lopez", 1, "4738241A"),
+("4738242B", "Pedo lopez", 1, "4738242B"),
+("4738244C", "Juan lopez", 1, "4738244C"),
+("4738245D", "Alberto lopez", 1, "4738245D"),
+("4738743E", "Nuria lopez", 1, "4738743E"),
+("4738247F", "Maria lopez", 1, "4738247F"),
+("4382143G", "Mar lopez", 1, "4382143G"),
+("4738249H", "Mariona lopez", 1, "4738249H"),
+("473124I", "Jose lopez", 1, "4731243I");
