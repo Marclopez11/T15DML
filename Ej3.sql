9c6877ff229c203f7949e02e26257b10d66e3e83
Marc Lopez <99056015+Marclopez11@users.noreply.github.com>
2022-05-01 15:15:23 +0200

    Ej3 Terminado

---
+++ Ej3.sql
@@ -0,0 +1,43 @@
+﻿INSERT INTO cientificos (dni, nomapels)
+VALUES ("4719147X", "Marc lopez"),
+("9849799G", "Marc lopez"),
+("3115657V", "Marc lopez"),
+("8154144L", "Marc lopez"),
+("1848233Y", "Marc lopez"),
+("2950638T", "Marc lopez"),
+("1242760F", "Marc lopez"),
+("3597496A", "Marc lopez"),
+("8568474Y", "Marc lopez"),
+("3901027Y", "Marc lopez");
+
+
+
+
+
+
+INSERT INTO proyecto (id, nombre,horas)
+VALUES ("vked", "Marc",4),
+ ("eler", "Marc",4),
+ ("grsh", "Marc",4),
+ ("uths", "Marc",4),
+ ("ytef", "Marc",4),
+ ("jryf", "Marc",4),
+ ("wytf", "Marc",4),
+ ("ytr4", "Marc",4),
+ ("yte5", "Marc",4),
+ ("rewg", "Marc",4);
+
+
+
+
+INSERT INTO asignado_a (cientifico, proyecto)
+VALUES ("9849799G", "rewg"),
+("3115657V", "yte5"),
+("8154144L", "ytr4"),
+("8568474Y", "wytf"),
+("3901027Y", "jryf"),
+("1848233Y", "uths"),
+("3597496A", "grsh"),
+("1242760F", "eler"),
+("2950638T", "rewg"),
+("9849799G", "ytef");
