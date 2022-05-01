361a4ca9059cc68a1e19ab6a12eeb7e7de39a929
Marc Lopez <99056015+Marclopez11@users.noreply.github.com>
2022-05-01 15:42:10 +0200

    Ej4

---
+++ Ej4.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO cajeros (nomapels)
+VALUES ( "Marc lopez"), ( "Marc lopez"),( "Marc lopez"),( "Marc lopez"),( "Marc lopez"),
+( "Marc lopez"),( "Marc lopez"),( "Marc lopez"),( "Marc lopez"),( "Marc lopez");
+
+
+INSERT INTO productos (nombre,precio)
+VALUES ( "Tv",20), ( "Tv",20), ( "Tv",20), ( "Tv",20), ( "Tv",20), ( "Tv",20),
+( "Tv",20), ( "Tv",20), ( "Tv",20), ( "Tv",20);
+
+
+INSERT INTO maquinas_registradas (piso)
+VALUES (20), (20), (20), (20), (20), (20), (20), (20), (20), (20);
+
+
+INSERT INTO venta (cajero,maquina,producto)
+VALUES (1,2,3),(2,2,3),(1,1,3),(1,3,3),(3,2,5),(6,4,3),(5,2,8),(4,5,3),(8,5,3),(8,7,3);
